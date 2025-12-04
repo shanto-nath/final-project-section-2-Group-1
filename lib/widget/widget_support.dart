4f6b78bf3fcd21d85cd204f9c4156cb0125ac646
Shanto Deb Nath <119739971+shanto-nath@users.noreply.github.com>
2025-12-04 12:41:24 +0600

    Widget Update

---
+++ lib/widget/widget_support.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppWidget {
+  static TextStyle boldTextFieldStyle() {
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Poppins');
+  }
+}
