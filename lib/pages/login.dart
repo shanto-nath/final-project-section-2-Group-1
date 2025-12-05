41504b1b6e6e34e0d86fa9646d2217fbe410dfba
Shanto Deb Nath <119739971+shanto-nath@users.noreply.github.com>
2025-12-05 20:53:11 +0600

    Media Query

---
+++ lib/pages/login.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({super.key});
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Color(0xFFff5c30), Color(0xFFe74b1a)],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height/3),
+              height: MediaQuery.of(context).size.height/2,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(color: Colors.cyanAccent, borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+              child: Text(''),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
