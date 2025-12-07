3e5181e6cb4d38b506ffeaeda3045452d333cbed
Promit polok <promitchowhan73@gmail.com>
2025-12-07 16:27:23 +0600

    Fixing Error

--- lib/widget/content_model.dart
+++ lib/widget/content_model.dart
@@ -2,22 +2,27 @@ class UnboardingContent {
   String image;
   String title;
   String description;
-  UnboardingContent(
-      {required this.description, required this.image, required this.title});
+  UnboardingContent({
+    required this.description,
+    required this.image,
+    required this.title,
+  });
 }
 
 List<UnboardingContent> contents = [
   UnboardingContent(
-      description: 'Pick your food from our menu\n More than 35 times',
-      image: "images/screen1.png",
-      title: 'Select from Our\n Best Menu'),
+    description: 'Pick your food from our menu\n More than 35 times',
+    image: "images/screen1.png",
+    title: 'Select Item 123........',
+  ),
   UnboardingContent(
-      description:
-          'You can pay cash on delivery and\n Card payment is available',
-      image: "images/screen2.png",
-      title: 'Easy and Online Payment'),
+    description: 'You can pay cash on delivery and\n Card payment is available',
+    image: "images/screen2.png",
+    title: 'Easy and Online Payment',
+  ),
   UnboardingContent(
-      description: 'Deliver your food at your\n Doorstep',
-      image: "images/screen3.png",
-      title: 'Quick Delivery at\n    Your Doorstep')
+    description: 'Deliver your food at your\n Doorstep',
+    image: "images/screen3.png",
+    title: 'Quick Delivery at\n    Your Doorstep',
+  ),
 ];
