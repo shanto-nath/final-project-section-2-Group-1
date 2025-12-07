3f30ba2d58d5128239be16f1685b02e44401b170
Promit polok <promitchowhan73@gmail.com>
2025-12-07 16:51:54 +0600

    Firebase Integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:testproject/firebase_options.dart';
 import 'package:testproject/pages/login.dart';
 import 'package:testproject/pages/onboard.dart';
 import 'package:testproject/pages/signup.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
